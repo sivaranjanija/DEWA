ba4922a0be419a2ed00edfbc04b6e78516a12021
Sivaranjani <sivaranjani.jayaprakash@gmail.com>
2021-02-21 17:45:40 +0400

    Working model

--- Solution2/warthog_navigation/src/waypoint_navigation.cpp
+++ Solution2/warthog_navigation/src/waypoint_navigation.cpp
@@ -7,12 +7,11 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int x[5] = { -4, 2, 4, 2, 4}; 
-int y[5] = {-4, -4, 0 , 4, 4};
-//float z[5] = {0, 0.47, 0.78, 1, -0.81};
-float w[5] = {1, 1, 1, 1, 1};
-
-int lenght0fWaypoints = 5;
+float x[6] = {1.5, 5, 8, 5, 1.5, 0}; 
+float y[6] = {1.5, 1.5, 5 , 8.5, 8.5, 5};
+float z[6] = {0.267, 0.730, 0.955, 0.947, -0.758, -0.327};
+float w[6] = {0.964, 0.683, 0.296, -0.320, 0.652, 0.945};
+int lenght0fWaypoints = 6;
 
 int main(int argc, char** argv)
 {
@@ -30,21 +29,24 @@ int main(int argc, char** argv)
 
   goal.target_pose.header.frame_id = "odom";
   goal.target_pose.header.stamp = ros::Time::now();
-  for( int i = 0; i < lenght0fWaypoints; i = i + 1 ) 
+  for(int numTimes = 0; numTimes < 2; numTimes++)
   {
-    goal.target_pose.pose.position.x = x[i];
-    goal.target_pose.pose.position.y = y[i];
-    //goal.target_pose.pose.orientation.z = z[i];
-    goal.target_pose.pose.orientation.w = w[i];
-    ROS_INFO("Sending goal");
-    ac.sendGoal(goal);
+    for( int i = 0; i < lenght0fWaypoints; i = i + 1 ) 
+    {
+      goal.target_pose.pose.position.x = x[i];
+      goal.target_pose.pose.position.y = y[i];
+      goal.target_pose.pose.orientation.z = z[i];
+      goal.target_pose.pose.orientation.w = w[i];
+      ROS_INFO("Sending goal");
+      ac.sendGoal(goal);
 
-    ac.waitForResult();
+      ac.waitForResult();
  
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_INFO("Success");
-    else
-      ROS_INFO("Failure");
+      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Success");
+      else
+        ROS_INFO("Failure");
+    }
   }
   return 0;
 }
